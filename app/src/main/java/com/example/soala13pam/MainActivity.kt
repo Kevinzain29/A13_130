31631739cc9e149a183c51b11c2660ae04daa1de
Kevinzain29 <ahmad.kevin.ft22@mail.umy.ac.id>
2025-01-28 23:03:33 +0700

    last

---
+++ app/src/main/java/com/example/soala13pam/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.soala13pam
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.soala13pam.ui.theme.SoalA13PAMTheme
+import com.example.soala13pam.ui.view.peserta.PesertaHomeScreen
+import com.example.soala13pam.ui.viewmodel.peserta.PesertaApp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SoalA13PAMTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    PesertaApp(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SoalA13PAMTheme {
+        Greeting("Android")
+    }
+}
